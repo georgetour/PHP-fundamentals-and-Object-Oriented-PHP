86d99343c7894e69b4b8df7773db0b346f817ee6
George Tourtsinakis <tourtsinakis@outlook.com>
2017-08-13 00:08:39 +0300

    Save method
    - Save method created for photo upload
    - HTML upload form
    - Not tested

--- Gallery-System-OOP/admin/includes/photo.php
+++ Gallery-System-OOP/admin/includes/photo.php
@@ -36,7 +36,7 @@ class Photo extends Db_object{
 	//This is passing $_FILES['uploaded_file'] as an argument
 	public function set_file($file){
 		
-		if(empty($file)||!$file||!is_array($file){
+		if(empty($file)|| !$file|| !is_array($file)){
 			
 			$this->errors[] = "There was no file uploaded here";
 			return false;
@@ -50,22 +50,65 @@ class Photo extends Db_object{
 		
 			$this->filename = basename($file['name']);
 			$this->tmp_path = $file['tmp_name'];
-			this->type = $file['type'];
+			$this->type = $file['type'];
 		
 		}
 		
 		
 		
 	}
+	
+	//Save file info to database
+	public function save(){
+		
+		if($this->id){
+			$this->update();
+		}
+		
+		else{
+			
+			if(!empty($this->errors)){
+				return false;
+			}
+			
+			if(empty($this->filename)||empty($this->tmp_path)){
+				$this->errors[] = "the file isn't available";
+				return false;
+				
+			}
+			
+			$target_path = SITE_ROOT . DS . $this->upload_directory . DS . $this->filename ;
+			
+			if(file_exists($target_path)){
+				$this->errors[] = "The file{} exists";
+				return false;
+			}
+			
+			//PHP function that will that takes filename tmp path and the destination
+			if(move_uploaded_file($this->tmp_path,$target_path)){
+				
+				if($this->create()){
+					unset($this->tmp_path);
+					return true;
+				}
 
-
-
-
-
-
-
-
-
+				
+			}else{
+				
+				$this->errors[]=" Problem with the folder permissions ";
+				return false;
+			}
+			
+			
+			$this->create();
+			
+		}
+		
+		
+		
+	}
+	
+	
 
 
 
--- Gallery-System-OOP/admin/upload.php
+++ Gallery-System-OOP/admin/upload.php
@@ -1,5 +1,10 @@
 <?php include("includes/header.php"); ?>
 
+
+<?php if(!$session->is_signed_in()){redirect("login.php"); }  ?>
+
+
+
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <!--Top navabar-->
@@ -20,14 +25,25 @@
                            Upload
                             <small>Subheading</small>
                         </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
+						
+						<div class="col-md-3">
+							<form action="" method="post" enctype="multipart/form-data">
+							
+								<div class="form-group">
+									<label for="title"> Picture title : </label>
+									<input id="title" type="text" name="title" class="form-control">
+								</div>
+
+								<div class="form-group">
+									<label for="file_upload"> File_upload : </label>
+									<input id="file_upload" type="file" name="file_upload" class="form-control">
+								</div>
+								
+								<input type="submit" name="submit">
+								
+							
+							</form>
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->
